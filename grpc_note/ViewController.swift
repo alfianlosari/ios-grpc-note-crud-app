a264501396c8a119ddfb745524215335e2084463
Alfian Losari <alfianlosari@gmail.com>
2018-09-08 15:39:43 +0700

    Initial Commit

---
+++ grpc_note/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  grpc_note
+//
+//  Created by Alfian Losari on 8/9/18.
+//  Copyright © 2018 Alfian Losari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
